950d5c5419f8d546fd56adfda2338f9168a273ef
shadan behzadian <shadan@shadans-MacBook-Pro.local>
2019-02-20 12:55:39 +0100

    modified server.js

--- server.js
+++ server.js
@@ -61,20 +61,20 @@ const port = process.env.PORT || 5000;
 // app.listen(port, () => console.log(`server started on port  ${port}`));
 
 //Static file declaration
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(__dirname));
 
-//production mode
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-  //
-  app.get("*", (req, res) => {
-    res.sendfile(path.join((__dirname = "client/build/index.html")));
-  });
-}
-//build mode
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/public/index.html"));
-});
+// //production mode
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "client/build")));
+//   //
+//   app.get("*", (req, res) => {
+//     res.sendfile(path.join((__dirname = "client/build/index.html")));
+//   });
+// }
+// //build mode
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname + "/client/public/index.html"));
+// });
 
 //start server
 app.listen(port, (req, res) => {
