fe3ea330db843963dd4e3b24c6aeffc6ed9edb5d
shadan behzadian <shadan@shadans-MacBook-Pro.local>
2019-03-08 15:34:32 +0100

    ready to use enzyme

--- client/src/components/WelcomeMessage.js
+++ client/src/components/WelcomeMessage.js
@@ -1,23 +1,10 @@
 import React, { Fragment } from "react";
 
-const styles = {
-  heading: {
-    color: "#fff",
-    textAlign: "center",
-    marginBottom: theme.margin * 2
-  },
-  logo: {
-    width: 250,
-    heading: 250,
-    objectFit: "cover"
-  }
-};
-
 const WelcomeMessage = imgPath => {
   return (
     <Fragment>
-      <h1 style={styles.heading}>Welcome To</h1>
-      <img src={imgPath} alt="app logo" style={styles.logo} />
+      <h1>Welcome To</h1>
+      <img src={imgPath} alt="app logo" />
     </Fragment>
   );
 };
--- client/src/components/WelcomeMessage.test.js
+++ client/src/components/WelcomeMessage.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 
 // Components
-import WelcomeMessage from "./WelcomeMessage";
+import WelcomeMessage from "./WelcomeMessage.js";
 
 function setup() {
   const props = {
